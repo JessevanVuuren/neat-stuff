d3854c12cb4ccf527d5486868529c796e551364f
Jesse van Vuuren <s1135034@student.hsleiden.nl>
2025-05-13 23:24:09 +0200

    info, utils and dome populaiton stuf

--- python/libs/utils.py
+++ python/libs/utils.py
@@ -2,8 +2,7 @@ from typing import Protocol, Self, List, Type, Sequence, TypeVar, Optional, Tupl
 from dataclasses import dataclass, field  # type: ignore
 from abc import ABC, abstractmethod # type: ignore
 from enum import Enum, EnumMeta  # type: ignore
-import pygame
-
+from pygame import Vector2
 
 class MetaEnum(EnumMeta):
     def __contains__(cls, item): # type: ignore
@@ -17,13 +16,14 @@ class MetaEnum(EnumMeta):
 class BaseEnum(Enum, metaclass=MetaEnum):
     pass
 
-def tuple_2_vec2(t: Tuple[int, int]) -> pygame.Vector2:
-    return pygame.Vector2(t[0], t[1])
+def tuple_2_vec2(t: Tuple[int, int]) -> Vector2:
+    return Vector2(t[0], t[1])
 
 
-def vec2_2i_tuple(v: pygame.Vector2) -> Tuple[int, int]:
+def vec2_2i_tuple(v: Vector2) -> Tuple[int, int]:
     return (int(v.x), int(v.y))
 
 
-def vec2_2f_tuple(v: pygame.Vector2) -> Tuple[float, float]:
+def vec2_2f_tuple(v: Vector2) -> Tuple[float, float]:
     return (v.x, v.y)
+
